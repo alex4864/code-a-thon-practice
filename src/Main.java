0b61fa2a5b1d654ed785c12c23a22ede5faaf518
Alex <awarrington0895@gmail.com>
2016-10-24 17:52:31 -0500

    Added list of function calls to Main

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,12 @@ public class Main
 	{
 		Intro intro = new Intro();
 		
+		//Function calls to list our the intros
+//		System.out.println(intro.andrew());
+//		System.out.println(intro.zach());
+//		System.out.println(intro.alexW());
+//		System.out.println(intro.alexS());
+//		System.out.println(intro.justin());
 		
 
 	}
