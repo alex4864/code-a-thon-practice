964f7ac0de162a2b0c69c5d9a698e1713f867c8d
alex4864 <shadleyalex@gmail.com>
2016-10-24 20:24:28 -0500

    Adds Alex Shadley function

--- src/Intro.java
+++ src/Intro.java
@@ -1,10 +1,13 @@
 
-public class Intro 
+public class Intro
 {
 	public Intro()
 	{
-		
+
 	}
-	
-	
+
+	public String alexS()
+	{
+		return "Hello, I'm Alex Shadley.  I play the Mellophone in KU's Marching Band, and some of my favorite movies are Office Space, Airplane!, and The Naked Gun.";
+	}
 }
